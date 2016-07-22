a3f3ed2ca9e587c9382138310a82f8a6c295bdcf
like-all <like-all@yandex.com>
2016-07-22 18:37:49 +0300

    Left-pad!

--- src/zadroteeque/zadroteeque.sh
+++ src/zadroteeque/zadroteeque.sh
@@ -4,6 +4,13 @@ source /usr/lib/leicht/leicht.sh
 
 case `echo $4 | cut -f 1 -d ' '` in
     "/listchats")
-    leicht_send_message "$2" "$3" "`cat /opt/zadroteeque/data/chat-list.txt`" "false" "/var/run/zadroteeque.sock"
-    ;;
+        leicht_send_message "$2" "$3" "`cat /opt/zadroteeque/data/chat-list.txt`" "false" "/var/run/zadroteeque.sock"
+        ;;
+    "/leftpad")
+        str=`echo $4 | cut -f 3- -d ' '`
+        pad=`echo $4 | cut -f 2 -d ' '`
+        let "num = ${#str} - $pad"
+        v=$(printf "%-${num}s" " ")
+        leicht_send_message "$2" "$3" "`echo \"${v// / }\"$str`" "false" "/var/run/zadroteeque.sock"
+        ;;
 esac
